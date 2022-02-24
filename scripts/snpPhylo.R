7809144ca0173ec4e88051c3e21e97af25f268d8
ArthurVM <ArthurVM@github.com>
2022-02-24 13:25:32 +0000

    snp phylo script

--- scripts/snpPhylo.R
+++ scripts/snpPhylo.R
@@ -3,6 +3,11 @@ library("adegenet")
 library("ape")
 library("optparse")
 library("glue")
+library("wordcloud")
+library("tidyverse")  # data manipulation
+library("cluster")    # clustering algorithms
+library("factoextra") # clustering algorithms & visualization
+library("gridExtra")
 
 parseArgs <- function() {
   ## Simple argument parser
@@ -23,25 +28,55 @@ parseArgs <- function() {
   return(opt)
 }
 
-makePhylo <- function(vcf) {
-  vcf <- "/home/amorris/BioInf/Parapipe/test_wetrunBIG_parv/GVCFs/merged.vcf"
+gind <- function(vcf) {
+  vcf <- "/home/amorris/BioInf/Parapipe/report_WD/merged.vcf"
   gind <- vcfR2genind(read.vcfR( vcf , verbose = FALSE ))
   row.names(gind@tab)
-  D <- dist(tab(gind))
+  
+  return(gind)
+}
+
+makePhylo <- function(D) {
   tre <- nj(D)
   for ( s in tre$"tip.label" ) {
-    fout <- glue("/home/amorris/BioInf/Parapipe/test_wetrunBIG_parv/{s}.pdf")
-    pdf(fout, width = 10, height = 10)
+    fout <- glue("/home/amorris/BioInf/Parapipe/report_WD/{s}.png")
+    png(fout, width = 700, height = 700)
     plot(tre, type="unrooted", edge.w=2, tip.color = c("red")[(tre$"tip.label"!=s)+1])
     edgelabels(tex=round(tre$edge.length,1), bg=rgb(.8,.8,1,.8))
     dev.off()
   }
 }
 
+makePCAplot <- function(D) {
+  pco1 <- dudi.pco(D, scannf=FALSE,nf=2)
+  s.label(pco1$li*1.1, clab=0, pch="")
+  textplot(pco1$li[,1], pco1$li[,2], words=rownames(pco1$li),
+           cex=1.0, new=FALSE, xpd=TRUE)
+  title("Principal Coordinate Analysis\n-based on SNP distances-")
+}
+
+kmeanscluster <- function(gind) {
+  gind.mat <- as.matrix(gind$tab)
+  gind.mat[is.na(gind.mat)] <- 0
+  gind.mat[gind.mat==2] <- 1
+  filtered.mat <- gind.mat[ , which(apply(gind.mat, 2, var) != 0)]
+  wss.p <- fviz_nbclust(filtered.mat, kmeans, method = "wss")
+  grid.arrange(wss.p, nrow = 1)
+  sc.km <- kmeans(filtered.mat, centers = 3, nstart =5)
+  p2 <- fviz_cluster(sc.km, data = filtered.mat, labelsize=0, main="k=2")
+  grid.arrange(p2, nrow = 1)
+}
+
 main <- function(){
   ## Main function
-  opt <- parseArgs()
-  makePhylo(opt$vcf)
+  ## opt <- parseArgs()
+  gind <- vcf2dist(opt$vcf)
+  D <- dist(tab(gind))
+  makePhylo(D)
+  makePCAplot(D)
+  
+  kmeanscluster(gind)
+  
 }
 
 main()
